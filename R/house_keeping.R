35005ebc4f56caf2c8bbd527a038be784e1dfcd4
Hung Pham <58577711+hhp94@users.noreply.github.com>
2022-11-04 20:12:20 -0400

    Added the house_keeping.R script

---
+++ R/house_keeping.R
@@ -0,0 +1,7 @@
+# c("arrow","DBI","dbplyr","dials","dplyr","glue","hardhat","jsonlite","parsnip",
+# "purrr","R6","recipes","rlang","RSQLite","stats","tidyr","withr", "kernlab")
+
+# c("covr", "dm", "here", "knitr", "rmarkdown", "testthat")
+
+# Depends:
+#   R (>= 2.10)
